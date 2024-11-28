44263dadebbe80c0ad07f6319205380d7e8e8b4b
yanJuicy <hanjub3@gmail.com>
2024-11-29 00:55:35 +0900

    반복문으로 영화 정보 표시

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
 <template>
   <h1>Hello Vue</h1>
-  <div>
-    <h3 class="bg-yellow" :style="textRed">{{ title }}</h3>
-    <p>개봉: {{ year }}</p>
-    <p>장르: {{ category}}</p>
+  <div v-for="(movie, i) in data" :key="i">
+    <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
+    <p>개봉: {{ movie.year }}</p>
+    <p>장르: {{ movie.category }}</p>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    data() {
-      return {
-        title: "노량",
-        year: 2023,
-        category: "액션, 드라마",
-        textRed: "color: red"
-      }
+export default {
+  name: 'App',
+  data() {
+    return {
+      data: [
+        {
+          title: "노량",
+          year: 2023,
+          category: "액션, 드라마",
+          textRed: "color: red"
+        },
+        {
+          title: "아쿠아맨과 로스트 킹덤",
+          year: 2023,
+          category: "액션, 판타지, 어드벤처"
+        },
+        {
+          title: "3일의 휴가",
+          year: 2023,
+          category: "판타지, 드라마"
+        }
+      ]
     }
   }
+}
 </script>
 
 <style>
-  .bg-yellow {
-    background: gold;
-    padding: 10px;
-  }
+.bg-yellow {
+  background: gold;
+  padding: 10px;
+}
 </style>
